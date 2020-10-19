8c4a8e75f465763b7e2e2b57ab420e05e877ec66
黄小刚 <664936010@qq.com>
2020-10-19 11:23:36 +0800

    添加项目文件。

---
+++ Function.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI.Events;
+
+namespace Bridge
+{
+    sealed class Function
+    {
+        //单例模式设计
+        private static Function instance = new Function();
+        private Function() { }
+        public static Function GetFunction()
+        {
+            return instance;
+        }
+        //进行各种按钮的属性设置
+        public void AddButtonIteam(ref RibbonPanel ribbon, string buttonType, string tabName,string buttonText,string assemblyName,string className, string largeImageuri, string toolTip, string longDescription)
+        {
+            
+            switch (buttonType)
+            {
+                case "PushButton":
+                    PushButtonData pbd = new PushButtonData(tabName, buttonText, assemblyName, className);//tabName可用可不用？
+                    SetPushButtonAttribute(ref ribbon, pbd, largeImageuri, toolTip, longDescription);
+                    break;
+                case "PulldownButton":
+                    PulldownButtonData pdbd = new PulldownButtonData("柱", "柱");
+                    SetPulldownButtonAttribute(ref ribbon, pdbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                case "SplitButton":
+                    SplitButtonData sbd = new SplitButtonData("板","板");
+                    //SetButtonAttribute(pbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                case "ComboBox":
+                    ComboBoxData cbd = new ComboBoxData("");
+                    //SetButtonAttribute(pbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                case "RadioButtonGroup":
+                    RadioButtonGroupData rbgd = new RadioButtonGroupData("");
+                    //SetButtonAttribute(pbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                case "ToggleButton":
+                    ToggleButtonData tbd = new ToggleButtonData("","");
+                    //SetButtonAttribute(pbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                case "TextBox":
+                    TextBoxData textbd = new TextBoxData("");
+                    //SetButtonAttribute(pbd, largeImageuri, toolTip, longDescription);
+
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetPushButtonAttribute(ref RibbonPanel ribbon,PushButtonData pbd, string largeImageuri, string toolTip, string longDescription)
+        {
+            SetCommonAttribute(ref pbd, largeImageuri, toolTip, longDescription);
+            RibbonButton ribbonButton = ribbon.AddItem(pbd) as PushButton;
+        }
+        public void SetCommonAttribute(ref PushButtonData pbd, string largeImageuri, string toolTip, string longDescription)
+        {
+            pbd.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            pbd.ToolTip = toolTip;
+            pbd.LongDescription = longDescription;
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetPulldownButtonAttribute(ref RibbonPanel ribbon, PulldownButtonData pdb, string largeImageuri, string toolTip, string longDescription)
+        {
+            PushButtonData p1 = new PushButtonData("test1","test1",@"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1");
+            PushButtonData p2 = new PushButtonData("test2","test2",@"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1");
+            PushButtonData p3 = new PushButtonData("test3","test3",@"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1");
+            pdb.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            pdb.ToolTip = toolTip;
+            pdb.LongDescription = longDescription;
+            PulldownButton pulldown = ribbon.AddItem(pdb) as PulldownButton;
+            //在PulldownButton中添加PushButton
+            SetCommonAttribute(ref p1, largeImageuri,toolTip,longDescription);//后面根据需求不是使用这几个参数，单独增加参数
+            SetCommonAttribute(ref p2, largeImageuri, toolTip, longDescription);//后面根据需求不是使用这几个参数，单独增加参数
+            SetCommonAttribute(ref p3, largeImageuri, toolTip, longDescription);//后面根据需求不是使用这几个参数，单独增加参数
+            pulldown.AddPushButton(p1);
+            pulldown.AddPushButton(p2);
+            pulldown.AddPushButton(p3);
+            
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetSplitButtonAttribute(SplitButton p, string largeImageuri, string toolTip, string longDescription)
+        {
+
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetComboboxAttribute(ComboBox p, string largeImageuri, string toolTip, string longDescription)
+        {
+
+            //p.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            p.ToolTip = toolTip;
+            p.LongDescription = longDescription;
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetRadioButtonGroupAttribute(RadioButtonGroup p, string largeImageuri, string toolTip, string longDescription)
+        {
+
+            //p.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            p.ToolTip = toolTip;
+            p.LongDescription = longDescription;
+        }
+        //设置PulldownButton类型的介绍信息
+        public void SetToggleButtonAttribute(PulldownButton p, string largeImageuri, string toolTip, string longDescription)
+        {
+
+            p.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            p.ToolTip = toolTip;
+            p.LongDescription = longDescription;
+        }       
+        //设置PulldownButton类型的介绍信息
+        public void SetTextBoxAttribute(PulldownButton p, string largeImageuri, string toolTip, string longDescription)
+        {
+
+            p.LargeImage = new BitmapImage(new Uri(largeImageuri));
+            p.ToolTip = toolTip;
+            p.LongDescription = longDescription;
+        }
+
+        //private void closed(object sender, ComboBoxDropDownClosedEventArgs e)
+        //{
+        //    TaskDialog.Show("关闭", "已关闭");
+        //}
+
+        //private void change(object sender, ComboBoxCurrentChangedEventArgs e)
+        //{
+        //    TaskDialog.Show("修改", "已修改");
+        //}
+    }
+}
---
+++ View.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * 此界面主要负责软件的界面实现功能
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI.Events;
+
+namespace Bridge
+{
+    enum ButtonType { PushButton, PulldownButton, SplitButton, Combobox, RadioButtonGroup, 
+        ToggleButton, TextBox, Separator
+    };
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class View : IExternalApplication
+    {
+        private const String BridgeName= "桥梁";
+   
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            Function functionInstance = Function.GetFunction();
+            //创建RibbonTab选项卡
+            application.CreateRibbonTab(BridgeName);
+
+            //创建RibbonPanel面板，并在面板上添加PushButton按钮
+            RibbonPanel panel1 = application.CreateRibbonPanel(BridgeName, "选择");
+            functionInstance.AddButtonIteam(ref panel1, ButtonType.PushButton.ToString(), BridgeName, "修改(MD)", @"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1", @"D:\Image\bridge\edit.jpg", "这是一段测试text", "这是将描述一段很详细的语句");
+
+            //创建RibbonPanel面板，并在面板上添加PulldownButton按钮
+            RibbonPanel panel2 = application.CreateRibbonPanel(BridgeName, "下部结构");
+            functionInstance.AddButtonIteam(ref panel2, ButtonType.PulldownButton.ToString(), BridgeName, "柱", @"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1", @"D:\Image\bridge\edit.jpg", "这是一段测试text", "这是将描述一段很详细的语句");
+
+            //创建RibbonPanel面板，并在面板上添加SplitButton按钮
+            RibbonPanel panel3 = application.CreateRibbonPanel(BridgeName, "基础部分");
+            functionInstance.AddButtonIteam(ref panel3, ButtonType.SplitButton.ToString(), BridgeName, "板", @"D:\workfile\HelloWorld\bin\Debug\HelloWorld.dll", "HelloWorld.Class1", @"D:\Image\bridge\edit.jpg", "这是一段测试text", "这是将描述一段很详细的语句");
+            
+            return Result.Succeeded;
+        }
+
+    }
+}
