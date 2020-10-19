e5755e109167d808730dadd4060c8495b6a42a34
黄小刚 <664936010@qq.com>
2020-10-19 13:57:50 +0800

    添加控件操作类

---
+++ Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI.Events;
+
+namespace Bridge
+{
+    class Controller : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+            Document doc = uIDocument.Document;
+            //对选中元素进行新增操作
+            //ICollection<ElementId> ids = doc.
+            //对选中元素进行删除操作
+            ICollection<ElementId> idsDel = doc.Delete(uIDocument.Selection.GetElementIds());
+            ////对选中元素进行查找操作
+            //ICollection<ElementId> ids2 = doc.Delete(uIDocument.Selection.GetElementIds());
+            ////对选中元素进行修改操作
+            //ICollection<ElementId> ids3 = doc.Delete(uIDocument.Selection.GetElementIds());
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CreateItem2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI.Events;
+
+namespace Bridge
+{
+    class CreateItem2
+    {
+    }
+}
